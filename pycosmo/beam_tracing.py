Fixed issue in power density normalization in PPI/RHI extractioN

pycosmo/beam_tracing.py
@@ -82,31 +82,32 @@ def _piecewise_linear(x,y):
     return ufunclike
 
 def integrate_quad(list_GH_pts):
-    num_beams=len(list_GH_pts)
-    
-    list_variables=list_GH_pts[0].values.keys()
+    n_beams = len(list_GH_pts)
+    n_gates = len(list_GH_pts[0].mask)
+    list_variables = list_GH_pts[0].values.keys()
  
     # Sum the mask of all beams to get overall mask
-    mask=np.zeros(num_beams,) # This mask serves to tell if the measured point is ok, or below topo or above COSMO domain
+    mask = np.zeros(n_gates,) # This mask serves to tell if the measured point is ok, or below topo or above COSMO domain
+    frac_pow = np.zeros(n_gates,) # Fraction of power received at antenna
     for i,p in enumerate(list_GH_pts):
         mask = _sum_arr(mask,p.mask) # Get mask of every Beam
-    mask/=float(num_beams) #mask == 1 means that every Beam is below TOPO, 
+        frac_pow = _sum_arr(frac_pow, (p.mask==0).astype(float)*p.GH_weight)
+
+    mask/=float(n_beams) #mask == 1 means that every Beam is below TOPO, 
                             #smaller than 0 that at least one Beam is above COSMO domain
     
     integrated_variables={}
     for k in list_variables:
         integrated_variables[k]=[float('nan')]
-        sum_weights=0
-        for i in list_GH_pts:
-            sum_weights+=i.GH_weight
-        for i in list_GH_pts:
+        for p in list_GH_pts:
             integrated_variables[k] = _nansum_arr(integrated_variables[k],
-                                        i.values[k]*i.GH_weight/sum_weights)
+                                        p.values[k]*p.GH_weight)
+        
         integrated_variables[k][np.logical_or(mask>1,mask<=-1)] = float('nan')
-        integrated_variables[k] /= sum_weights
-    
+        integrated_variables[k] /= frac_pow # Normalize by valid pts
+        
     # Get index of central beam
-    idx_0=int(num_beams/2)
+    idx_0=int(n_beams/2)
 
     heights_radar=list_GH_pts[idx_0].heights_profile
     distances_radar=list_GH_pts[idx_0].dist_profile
@@ -114,7 +115,7 @@ def integrate_quad(list_GH_pts):
     lons=list_GH_pts[idx_0].lons_profile
 
     integrated_beam = _Beam(integrated_variables,mask,lats, lons, distances_radar, heights_radar)
-    return integrated_beam
+    return integrated_beam, frac_pow
 
     
 def quad_pts_weights(beamwidth, npts_GH):

pycosmo/extract.py
@@ -486,6 +486,7 @@ def extract(variables, slice_type, idx, parallel = False):
         lats_scan = np.zeros((len(rangevec),len(azimuths)))
         heights_scan = np.zeros((len(rangevec),len(azimuths)))
         dist_ground = np.zeros((len(rangevec),len(azimuths)))
+        frac_power = np.zeros((len(rangevec),len(azimuths)))
         
         # Main loop
         for radial,az in enumerate(azimuths): # Loop on all radials
@@ -536,7 +537,7 @@ def extract(variables, slice_type, idx, parallel = False):
                             weights[i,j]))         
                         
             # Integrate all sub-beams
-            scan = integrate_quad(list_beams)    
+            scan, frac_pow = integrate_quad(list_beams)    
             
             # Add radial to slices
             for sli in slices: 
@@ -547,7 +548,8 @@ def extract(variables, slice_type, idx, parallel = False):
             lons_scan[:,radial] = scan.lons_profile
             heights_scan[:,radial] = scan.heights_profile
             dist_ground[:,radial] = scan.dist_profile
-        
+            frac_power[:,radial] = frac_pow
+            
         # Final bookkeeping
         for sli in slices:      
             sli.dim = sli.dim - 1
@@ -559,6 +561,7 @@ def extract(variables, slice_type, idx, parallel = False):
             sli.attributes['lat_2D'] = lats_scan
             sli.attributes['altitude'] = heights_scan
             sli.attributes['elevation'] = elevation
+            sli.attributes['fraction_power'] = frac_power
             sli.attributes['dist_ground'] = dist_ground
             sli.name+='_PPI_SLICE'
             sli.coordinates.pop('hyb_levels',None) # not needed anymore
@@ -660,7 +663,7 @@ def extract(variables, slice_type, idx, parallel = False):
         lats_scan = np.zeros((len(rangevec),len(elevations)))
         dist_ground = np.zeros((len(rangevec),len(elevations)))
         heights_scan = np.zeros((len(rangevec),len(elevations)))
-        
+        frac_power = np.zeros((len(rangevec),len(elevations)))
         
         # Main loop
         for radial,elev in enumerate(elevations): # Loop on all radials
@@ -712,7 +715,7 @@ def extract(variables, slice_type, idx, parallel = False):
                             weights[i,j]))         
                         
             # Integrate all sub-beams
-            scan = integrate_quad(list_beams)    
+            scan, frac_pow = integrate_quad(list_beams)    
             
             # Add radial to slices
             for sli in slices: 
@@ -723,6 +726,7 @@ def extract(variables, slice_type, idx, parallel = False):
             lons_scan[:,radial] = scan.lons_profile
             heights_scan[:,radial] = scan.heights_profile
             dist_ground[:,radial] = scan.dist_profile
+            frac_power[:,radial] = frac_pow
             
         # Final bookkeeping
         for sli in slices:      
@@ -735,6 +739,7 @@ def extract(variables, slice_type, idx, parallel = False):
             sli.attributes['lon_2D'] = lons_scan
             sli.attributes['lat_2D'] = lats_scan
             sli.attributes['altitude'] = heights_scan
+            sli.attributes['frac_power'] = frac_power
             sli.attributes['azimuth'] = azimuth
             sli.name+='_RHI_SLICE'
             sli.coordinates.pop('hyb_levels',None) # not needed anymore